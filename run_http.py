80746351f06f4b42d0ac59bd7108d6eaf40e7f6c
haniffelix68 <156521553+haniffelix68@users.noreply.github.com>
2024-06-14 06:14:29 +0700

    Update run_http.py

--- run_http.py
+++ run_http.py
@@ -78,12 +78,12 @@ async def main():
     #find user_id on the site in conlose localStorage.getItem('userId') (if you can't get it, write allow pasting)
     _user_id = input('Please Enter your user ID: ')
     #put the proxy in a file in the format socks5://username:password@ip:port or socks5://ip:port
-    r = requests.get("https://github.com/monosans/proxy-list/raw/main/proxies/http.txt", stream=True)
+    r = requests.get("https://github.com/haniffelix68/grass/raw/main/socks5.txt", stream=True)
     if r.status_code == 200:
-        with open('http.txt', 'wb') as f:
+        with open('socks5.txt', 'wb') as f:
             for chunk in r:
                 f.write(chunk)
-        with open('http.txt', 'r') as file:
+        with open('socks5.txt', 'r') as file:
                 socks5_proxy_list = file.read().splitlines()
     
     tasks = [asyncio.ensure_future(connect_to_wss('http://'+i, _user_id)) for i in socks5_proxy_list]
